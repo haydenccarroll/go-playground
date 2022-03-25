a1697fb1fa6ded79cc76d6528c69e0b2d5bb8e9d
Hayden Carroll <haydenccarroll@gmail.com>
2022-03-25 13:16:27 -0700

    added subtraction support.
    Current issues: cannot do double minus signs --
    cannot do parenthetic multiplication, it simply concatenates them together
    
    need to rework some stuff and make it cleaner, use slices probably

--- cmd/pemdasLogic.go
+++ cmd/pemdasLogic.go
@@ -8,9 +8,18 @@ import (
 	"strconv"
 )
 
-type operands struct {
-	str string
-	val int
+func IsOperatorToLeft(str string, strLoc int) bool {
+	if strLoc < 0 || strLoc >= len(str) {
+		fmt.Println("Error in IsOperatorToLeft. strLoc is not in string.")
+		os.Exit(1)
+	}
+	if strLoc == 0 {
+		return false
+	}
+	if str[strLoc-1] == '*' || str[strLoc-1] == '+' {
+		return true
+	}
+	return false
 }
 
 func IsOperand(str string) bool {
@@ -99,18 +108,18 @@ func MulDivPemdas(expression string) string {
 		leftOperandLoc, rightOperandLoc = FindLeftRightOperands(expression, mulLoc)
 		leftOperandStr = expression[leftOperandLoc:mulLoc]
 		rightOperandStr = expression[mulLoc+1 : rightOperandLoc+1]
-	} else {
+	} else if divLoc != -1 {
 		leftOperandLoc, rightOperandLoc = FindLeftRightOperands(expression, divLoc)
 		leftOperandStr = expression[leftOperandLoc:divLoc]
 		rightOperandStr = expression[divLoc+1 : rightOperandLoc+1]
 	}
 	leftOperandFloat, err := strconv.ParseFloat(leftOperandStr, 64)
 	if err != nil {
-		fmt.Printf("Error converting %s to float", leftOperandStr)
+		fmt.Printf("Error converting %s to float\n", leftOperandStr)
 	}
 	rightOperandFloat, err := strconv.ParseFloat(rightOperandStr, 64)
 	if err != nil {
-		fmt.Printf("Error converting %s to float", rightOperandStr)
+		fmt.Printf("Error converting %s to float\n", rightOperandStr)
 	}
 	if mulLoc != -1 {
 		result = fmt.Sprintf("%f", leftOperandFloat*rightOperandFloat)
@@ -121,12 +130,16 @@ func MulDivPemdas(expression string) string {
 	return expression
 }
 
-func AdditionPemdas(expression string) string {
+func AddSubPemdas(expression string) string {
 	// calculate addition
 	additionLoc := -1
+	subtractionLoc := -1
 	for i, c := range expression {
 		if c == '+' {
 			additionLoc = i
+			break
+		} else if c == '-' && !IsOperatorToLeft(expression, i) && i != 0 { // found binary subtraction operator
+			subtractionLoc = i
 			break
 		}
 	}
@@ -137,14 +150,29 @@ func AdditionPemdas(expression string) string {
 
 		leftOperandFloat, err := strconv.ParseFloat(leftOperandStr, 64)
 		if err != nil {
-			fmt.Printf("Error converting %s to float", leftOperandStr)
+			fmt.Printf("Error converting %s to float\n", leftOperandStr)
 		}
 		rightOperandFloat, err := strconv.ParseFloat(rightOperandStr, 64)
 		if err != nil {
-			fmt.Printf("Error converting %s to float", rightOperandStr)
+			fmt.Printf("Error converting %s to float\n", rightOperandStr)
 		}
 
 		result := fmt.Sprintf("%f", leftOperandFloat+rightOperandFloat)
+		expression = ReplaceSubExp(expression, result, leftOperandLoc, rightOperandLoc)
+	} else if subtractionLoc != -1 {
+		leftOperandLoc, rightOperandLoc := FindLeftRightOperands(expression, subtractionLoc)
+		leftOperandStr := expression[leftOperandLoc:subtractionLoc]
+		rightOperandStr := expression[subtractionLoc+1 : subtractionLoc+1]
+
+		leftOperandFloat, err := strconv.ParseFloat(leftOperandStr, 64)
+		if err != nil {
+			fmt.Printf("Error converting %s to float\n", leftOperandStr)
+		}
+		rightOperandFloat, err := strconv.ParseFloat(rightOperandStr, 64)
+		if err != nil {
+			fmt.Printf("Error converting %s to float\n", rightOperandStr)
+		}
+		result := fmt.Sprintf("%f", leftOperandFloat-rightOperandFloat)
 		expression = ReplaceSubExp(expression, result, leftOperandLoc, rightOperandLoc)
 	}
 	return expression
@@ -182,7 +210,7 @@ func ParenthesisPemdas(expression string) string {
 func RecursivePemdas(expression string) string {
 	expression = ParenthesisPemdas(expression)
 	expression = MulDivPemdas(expression)
-	expression = AdditionPemdas(expression)
+	expression = AddSubPemdas(expression)
 
 	_, err := strconv.ParseFloat(expression, 64)
 	if err == nil {
