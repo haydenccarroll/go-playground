c74ad11e450e132e425e4d281d4e8380b3b35d2d
Hayden Carroll <haydenccarroll@gmail.com>
2022-05-22 13:57:40 -0700

    added pemdas folder

--- pemas_cmdline_stuff/cmd/pemdas.go
+++ pemas_cmdline_stuff/cmd/pemdas.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright © 2022 Hayden Carroll (haydencarroll77@gmail.com)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
