e17455df5942714d7aed2b6f882bd7b644dca37e
Hayden Carroll <haydenccarroll@gmail.com>
2022-05-22 17:57:28 -0700

    added a bit to my website

--- html_website/main.go
+++ html_website/main.go
@@ -9,15 +9,31 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func main() {
+// The new router function creates the router and
+// returns it to us. We can now use this function
+// to instantiate and test the router outside of the main function
+func newRouter() *mux.Router {
 	r := mux.NewRouter()
-
 	r.HandleFunc("/", home).Methods("GET")
+	
+	// declare the directory filled with static files
+	staticFileDirectory := http.Dir("./assets/")
+	
+	// create a handler that does a /assets/randomAsset.html GET request
+	// need to strip prefix so it doesnt search for /assets/assets/file.html
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
 
+	// sticks the static file handler under the /assets/ page
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+
+	return r
+}
+
+func main() {
+	r := newRouter()
 	http.ListenAndServe(":5000", r)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Method)
-	fmt.Fprintf(w, "Get away from my home!")
+	fmt.Fprintf(w, "Hello World")
 }
--- html_website/main_test.go
+++ html_website/main_test.go
@@ -1,43 +1,40 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestHandler(t *testing.T) {
-	// we create a new HTTP request here. This request is going to be passed to
-	//   the handler.
-	// first arg is method, 2nd arg is the route, 3rd arg is request body (not implemented yet)
-	req, err := http.NewRequest("GET", "", nil)
+func TestRouterForHome(t *testing.T) {
+	// Instantiate the router
+	r := newRouter()
 
-	// there was an error creating the request, stop and fail test.
+	mockServer := httptest.NewServer(r)
+	res, err := http.Get(mockServer.URL + "")
+
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// golang's httptest library to create a http recorder. think of this recorder as a mini browser, it is
-	// the target of our http requests.
-	recorder := httptest.NewRecorder()
-
-	// create http handler for our handler function. argument is handler function
-	// defined in main.go.
-	hf := http.HandlerFunc(home)
-
-	hf.ServeHTTP(recorder, req)
-
-	// if we didnt get the right code from the html request
-	if status := recorder.Code; status != http.StatusOK {
-		t.Errorf("handler function returned wrong status code: got %v expected %v",
-			status, http.StatusOK)
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Status should be ok, got: %v", res.StatusCode)
 	}
 
-	expected := "Hello world!"
-	actual := recorder.Body.String()
-	if actual != expected {
-		t.Errorf("handler function returned wrong body: got %v expected %v",
-			actual, expected)
+	// close res.Body at end of test function
+	defer res.Body.Close()
+
+	//
+	bStr, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
 	}
 
+	resStr := string(bStr)
+	expected := "Hello World"
+
+	if resStr != expected {
+		t.Errorf("Response should be %s got %s instead.", expected, resStr)
+	}
 }
